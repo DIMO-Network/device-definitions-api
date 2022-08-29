01e8ee5ac5b3cb7129a323a9b8a0038baa99abc2
ricardo <chenton147@hotmail.com>
2022-08-28 19:53:01 -0500

    lint updated

--- internal/core/queries/get_dd_by_make_model_year.go
+++ internal/core/queries/get_dd_by_make_model_year.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+
 	"github.com/DIMO-Network/device-definitions-api/internal/core/common"
 	"github.com/DIMO-Network/device-definitions-api/internal/infrastructure/db/models"
 	"github.com/DIMO-Network/device-definitions-api/internal/infrastructure/db/repositories"
