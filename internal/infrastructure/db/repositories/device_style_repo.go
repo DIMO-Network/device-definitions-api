3d96699577b6044da4fa9418cdf9903c2ec196d0
Eduardo Rodríguez <eduardo.rodriguezz@outlook.com>
2022-09-21 17:20:10 -0500

    Device definition admin (#21)
    * add device definition dynamic filter endpoint
    
    * [fix] add filter by verified vin
    
    * [fix] add grpc service
    
    * [fix] fix casing
    
    * fix comments
    
    * [feat] add create styles grpc endpoint

---
+++ internal/infrastructure/db/repositories/device_style_repo.go
@@ -0,0 +1,43 @@
+//go:generate mockgen -source device_integration_repo.go -destination mocks/device_integration_repo_mock.go -package mocks
+
+package repositories
+
+import (
+	"context"
+
+	"github.com/DIMO-Network/device-definitions-api/internal/infrastructure/db/models"
+	"github.com/DIMO-Network/device-definitions-api/internal/infrastructure/exceptions"
+	"github.com/DIMO-Network/shared/db"
+	"github.com/volatiletech/sqlboiler/v4/boil"
+)
+
+type DeviceStyleRepository interface {
+	Create(ctx context.Context, deviceDefinitionID string, name string, externalStyleID string, source string, subModel string) (*models.DeviceStyle, error)
+}
+
+type deviceStyleRepository struct {
+	DBS func() *db.ReaderWriter
+}
+
+func NewDeviceStyleRepository(dbs func() *db.ReaderWriter) DeviceStyleRepository {
+	return &deviceStyleRepository{DBS: dbs}
+}
+
+func (r *deviceStyleRepository) Create(ctx context.Context, deviceDefinitionID string, name string, externalStyleID string, source string, subModel string) (*models.DeviceStyle, error) {
+
+	ds := &models.DeviceStyle{
+		DeviceDefinitionID: deviceDefinitionID,
+		Name:               name,
+		ExternalStyleID:    externalStyleID,
+		Source:             source,
+		SubModel:           subModel,
+	}
+	err := ds.Insert(ctx, r.DBS().Writer, boil.Infer())
+	if err != nil {
+		return nil, &exceptions.InternalError{
+			Err: err,
+		}
+	}
+
+	return ds, nil
+}
