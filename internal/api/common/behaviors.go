03b7c0777fcdb117ac1174f1e105167e92947f8d
ricardo <chenton147@hotmail.com>
2022-09-28 08:53:09 -0500

    lint fix

--- internal/api/common/behaviors.go
+++ internal/api/common/behaviors.go
@@ -3,6 +3,7 @@ package common
 import (
 	"context"
 	"fmt"
+
 	"github.com/DIMO-Network/device-definitions-api/internal/config"
 	"github.com/DIMO-Network/device-definitions-api/internal/infrastructure/exceptions"
 	"github.com/DIMO-Network/device-definitions-api/internal/infrastructure/metrics"
