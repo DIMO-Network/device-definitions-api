ad6769dee3cb6e0255b1db650a58d67b78ee6aa0
Dylan Moreland <dylan@dimo.zone>
2022-10-06 12:06:07 -0400

    Move to camel case for JSON blob

--- internal/infrastructure/elasticsearch/models/integration_features.go
+++ internal/infrastructure/elasticsearch/models/integration_features.go
@@ -1,6 +1,6 @@
 package models
 
 type DeviceIntegrationFeatures struct {
-	FeatureKey   string `json:"feature_key,omitempty"`
-	SupportLevel int8   `json:"support_level"`
+	FeatureKey   string `json:"featureKey"`
+	SupportLevel int8   `json:"supportLevel"`
 }
