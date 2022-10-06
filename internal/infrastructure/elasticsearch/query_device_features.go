a5733c049d67b6d17dc616f8a73538a033103013
Dylan Moreland <dylan@dimo.zone>
2022-10-06 12:31:55 -0400

    Try to incorporate regions

--- internal/infrastructure/elasticsearch/query_device_features.go
+++ internal/infrastructure/elasticsearch/query_device_features.go
@@ -6,20 +6,25 @@ import (
 	"github.com/pkg/errors"
 )
 
+type ElasticFilterResult struct {
+	DocCount int `json:"doc_count"`
+}
+
 type DeviceFeaturesResp struct {
 	Aggregations struct {
-		Features struct {
-			SumOtherDocCount int64 `json:"sum_other_doc_count"`
-			Buckets          []struct {
+		Integrations struct {
+			Buckets []struct {
 				Key               string `json:"key"`
-				DocumentCount     int64  `json:"doc_count"`
 				DeviceDefinitions struct {
-					SumOtherDocCount int64 `json:"sum_other_doc_count"`
-					Buckets          []struct {
-						Key      string `json:"key"`
-						DocCount int64  `json:"doc_count"`
-						Features struct {
-							Buckets map[string]map[string]int
+					Buckets []struct {
+						Key     string `json:"key"`
+						Regions struct {
+							Buckets []struct {
+								Key      string `json:"key"`
+								Features struct {
+									Buckets map[string]ElasticFilterResult
+								}
+							}
 						}
 					}
 				}
@@ -28,47 +33,60 @@ type DeviceFeaturesResp struct {
 	}
 }
 
-func (d *ElasticSearch) GetDeviceFeatures(envName, fields string) (DeviceFeaturesResp, error) {
-	url := fmt.Sprintf("%s/device-status-%s*/_search", d.BaseURL, envName)
+func (d *ElasticSearch) GetDeviceFeatures(envName, filterList string) (DeviceFeaturesResp, error) {
+	url := fmt.Sprintf("%s/device-status-%s-*/_search", d.BaseURL, envName)
 	body := `
-	{
-		"size":0,
-		"query":{
-		   "bool":{
-			  "must":[
-				 {
-					"exists":{
-					   "field":"data.deviceDefinitionId"
+{
+	"size": 0,
+	"query": {
+		"bool": {
+			"must": [
+				{
+					"exists": {
+						"field": "data.deviceDefinitionId"
 					}
-				 }
-			  ]
-		   }
-		},
-		"aggs":{
-		   "features":{
-			  "terms":{
-				 "field":"source",
-				 "size":1000
-			  },
-			  "aggs":{
-				 "deviceDefinitions":{
-					"terms":{
-					   "field":"data.deviceDefinitionId",
-					   "size":10000
-					},
-					"aggs":{
-					   "features":{
-						  "filters":{
-							"filters":` + fields + `
-						  }
-					   }
+				},
+				{
+					"exists": {
+						"field": "data.region"
 					}
-				 }
-			  }
-		   }
+				}
+			]
 		}
-	 }
-	`
+	},
+	"aggs": {
+		"integrations": {
+			"terms": {
+				"field": "source",
+				"size": 1000
+			},
+			"aggs": {
+				"deviceDefinitions": {
+					"terms": {
+						"field": "data.deviceDefinitionId",
+						"size": 10000
+					},
+					"aggs": {
+						"regions": {
+							"terms": {
+								"field": "data.region",
+								"size": 1000
+							},
+							"aggs": {
+								"features": {
+									"filters": {
+										"filters": ` + filterList + `
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+`
 
 	deviceFeatures := DeviceFeaturesResp{}
 
