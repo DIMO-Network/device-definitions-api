3f9da7235dc4bc178402984c90c87c88dffbf3ce
Allyson-English <allyson.english@gmail.com>
2022-10-20 16:32:08 -0600

    load fuel api images to table

--- internal/infrastructure/db/migrations/20221020114358_create_images_table.sql
+++ internal/infrastructure/db/migrations/20221020114358_create_images_table.sql
@@ -2,17 +2,23 @@
 -- +goose StatementBegin
 SELECT 'up SQL query';
 
+
 CREATE TABLE IF NOT EXISTS images
 (
-    id character(27) NOT NULL,
-    device_definition_id NOT NULL,
+    id CHAR(27) PRIMARY KEY,
+    device_definition_id char(27) NOT NULL,
     fuel_api_id TEXT,
     width INT,
     height INT,
     source_url TEXT NOT NULL,
     dimo_s3_url TEXT,
-    color TEXT NOT NULL,
+    color TEXT NOT NULL
 );
+
+ALTER TABLE images 
+ADD CONSTRAINT fkey_device_definition_id 
+FOREIGN KEY (device_definition_id) 
+REFERENCES device_definitions_api.device_definitions(id) ON DELETE CASCADE;
 -- +goose StatementEnd
 
 -- +goose Down
