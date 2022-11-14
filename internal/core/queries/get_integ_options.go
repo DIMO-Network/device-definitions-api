326715d6c6b98d22b9bd6dfda2c9e1c1c9a4b19a
James Reategui <jreate@me.com>
2022-11-14 12:49:51 -0500

    implement querying

--- internal/core/queries/get_integ_options.go
+++ internal/core/queries/get_integ_options.go
@@ -2,8 +2,12 @@ package queries
 
 import (
 	"context"
+	"fmt"
+	"github.com/DIMO-Network/device-definitions-api/internal/infrastructure/exceptions"
+	p_grpc "github.com/DIMO-Network/device-definitions-api/pkg/grpc"
 	"github.com/DIMO-Network/shared/db"
 	"github.com/TheFellow/go-mediator/mediator"
+	"github.com/volatiletech/sqlboiler/v4/queries"
 )
 
 type GetIntegrationOptionsQuery struct {
@@ -24,10 +28,36 @@ func NewGetIntegrationOptionsQueryHandler(dbs func() *db.ReaderWriter) GetIntegr
 
 func (ch GetIntegrationOptionsQueryHandler) Handle(ctx context.Context, query mediator.Message) (interface{}, error) {
 	qry := query.(*GetIntegrationOptionsQuery)
-	// could pull this one from cache since doesn't change often
+	// could pull this one from cache since doesn't change often, although we have frontend caching from explorer
+	var options []*IntegrationOption
+	// raw query to keep performant
+	err := queries.Raw(`select di.integration_id, i.vendor, di.region from device_integrations di
+	join integrations i on i.id = di.integration_id
+	join device_definitions dd on dd.id = di.device_definition_id
+	where dd.device_make_id = $1
+	group by di.integration_id, i.vendor, di.region`, qry.MakeID).Bind(ctx, ch.DBS().Reader, &options)
 
-	// raw query
+	if err != nil {
+		return nil, &exceptions.InternalError{
+			Err: fmt.Errorf("failed to get integration options for make %s", qry.MakeID),
+		}
+	}
 
 	// build up grpc object
-	return qry, nil
+	resp := p_grpc.GetIntegrationOptionsResponse{IntegrationOptions: make([]*p_grpc.GetIntegrationOptionItem, len(options))}
+	for i, option := range options {
+		resp.IntegrationOptions[i] = &p_grpc.GetIntegrationOptionItem{
+			IntegrationId:     option.IntegrationID,
+			IntegrationVendor: option.IntegrationVendor,
+			Region:            option.Region,
+		}
+	}
+
+	return resp, nil
+}
+
+type IntegrationOption struct {
+	IntegrationID     string `boil:"integration_id"`
+	IntegrationVendor string `boil:"vendor"`
+	Region            string `boil:"region"`
 }
