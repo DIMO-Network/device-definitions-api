b01e07a05674fcc9531fe8ea5711eadbd8123ab7
James Reategui <jreate@me.com>
2022-11-15 11:48:32 -0500

    linter

--- internal/core/queries/device_compatibility.go
+++ internal/core/queries/device_compatibility.go
@@ -3,15 +3,15 @@ package queries
 import (
 	"context"
 	"fmt"
-	"github.com/DIMO-Network/device-definitions-api/internal/infrastructure/exceptions"
-	p_grpc "github.com/DIMO-Network/device-definitions-api/pkg/grpc"
-	"github.com/pkg/errors"
-	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 
 	"github.com/DIMO-Network/device-definitions-api/internal/infrastructure/db/models"
 	"github.com/DIMO-Network/device-definitions-api/internal/infrastructure/db/repositories"
+	"github.com/DIMO-Network/device-definitions-api/internal/infrastructure/exceptions"
+	p_grpc "github.com/DIMO-Network/device-definitions-api/pkg/grpc"
 	"github.com/DIMO-Network/shared/db"
 	"github.com/TheFellow/go-mediator/mediator"
+	"github.com/pkg/errors"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
 
 type GetDeviceCompatibilityQueryHandler struct {
