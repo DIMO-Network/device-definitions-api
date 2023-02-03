d0f9ffd5b7e6f54cccc3a1b35fed99b0fbc04df3
James Reategui <jreate@me.com>
2023-02-02 22:04:25 -0500

    add grpc to get definitions with a template set

---
+++ internal/core/queries/get_dd_with_hw_template.go
@@ -0,0 +1,56 @@
+package queries
+
+import (
+	"context"
+
+	"github.com/DIMO-Network/device-definitions-api/internal/infrastructure/db/models"
+	"github.com/DIMO-Network/shared/db"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
+
+	"github.com/DIMO-Network/device-definitions-api/pkg/grpc"
+	"github.com/TheFellow/go-mediator/mediator"
+	"github.com/rs/zerolog"
+)
+
+type GetDefinitionsWithHWTemplateQuery struct {
+}
+
+func (*GetDefinitionsWithHWTemplateQuery) Key() string {
+	return "GetDefinitionsWithHWTemplateQuery"
+}
+
+type GetDefinitionsWithHWTemplateQueryHandler struct {
+	dbs func() *db.ReaderWriter
+	log *zerolog.Logger
+}
+
+func NewGetDefinitionsWithHWTemplateQueryHandler(dbs func() *db.ReaderWriter, log *zerolog.Logger) GetDefinitionsWithHWTemplateQueryHandler {
+	return GetDefinitionsWithHWTemplateQueryHandler{
+		dbs: dbs,
+		log: log,
+	}
+}
+
+func (ch GetDefinitionsWithHWTemplateQueryHandler) Handle(ctx context.Context, query mediator.Message) (interface{}, error) {
+
+	response := &grpc.GetDevicesMMYResponse{}
+
+	all, err := models.DeviceDefinitions(
+		qm.Load(models.DeviceDefinitionRels.DeviceMake),
+		models.DeviceDefinitionWhere.HardwareTemplateID.IsNotNull()).All(ctx, ch.dbs().Reader)
+	if err != nil {
+		return nil, err
+	}
+	response.Device = make([]*grpc.GetDevicesMMYItemResponse, len(all))
+	for i, definition := range all {
+		response.Device[i] = &grpc.GetDevicesMMYItemResponse{
+			Make:               definition.R.DeviceMake.Name,
+			Model:              definition.Model,
+			Year:               int32(definition.Year),
+			Id:                 definition.ID,
+			HardwareTemplateId: definition.HardwareTemplateID.String,
+		}
+	}
+
+	return response, nil
+}
