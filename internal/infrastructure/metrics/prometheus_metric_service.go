b06e3974dea8bffbc942bc583010ab1409d916f4
James Reategui <jreate@me.com>
2023-04-24 10:29:02 -0400

    set prom prefix

--- internal/infrastructure/metrics/prometheus_metric_service.go
+++ internal/infrastructure/metrics/prometheus_metric_service.go
@@ -5,29 +5,31 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
+const serviceName = "device_definitions_api_"
+
 var (
 	Success = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "request_success_ops_total",
+		Name: serviceName + "request_success_ops_total",
 		Help: "Total execution",
 	}, []string{"method"})
 
 	InternalError = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "request_error_ops_total",
+		Name: serviceName + "request_error_ops_total",
 		Help: "Total execution",
 	}, []string{"method"})
 
 	BadRequestError = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "request_bad_ops_total",
+		Name: serviceName + "request_bad_ops_total",
 		Help: "Total execution",
 	}, []string{"method"})
 
 	ConflictRequestError = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "request_conflict_ops_total",
+		Name: serviceName + "request_conflict_ops_total",
 		Help: "Total execution",
 	}, []string{"method"})
 
 	NotFoundRequestError = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "request_notfound_ops_total",
+		Name: serviceName + "request_notfound_ops_total",
 		Help: "Total execution",
 	}, []string{"method"})
 )
