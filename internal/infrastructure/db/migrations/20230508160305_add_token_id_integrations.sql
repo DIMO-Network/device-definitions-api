15ebb37662f082eafcce83be6b2dd5d4ffcb3e6f
0xdev22 <doyin@dimo.zone>
2023-05-08 17:06:38 -0400

    Add tokenID to Integrations proto and column to integrations table

---
+++ internal/infrastructure/db/migrations/20230508160305_add_token_id_integrations.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+-- +goose StatementBegin
+SELECT 'up SQL query';
+SET search_path = device_definitions_api, public;
+ALTER TABLE integrations ADD token_id numeric(78,0) null;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+SET search_path = device_definitions_api, public;
+ALTER TABLE integrations drop column token_id;
+-- +goose StatementEnd
