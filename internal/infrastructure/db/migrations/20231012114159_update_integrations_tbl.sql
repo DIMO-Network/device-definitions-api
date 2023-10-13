a04b0d57d157bc4937ad190277add955df078525
Allyson-English <allyson.english@gmail.com>
2023-10-13 08:49:39 -0600

    points default 0; manufTID type int (mirrors token_id column type already in table) and fk

--- internal/infrastructure/db/migrations/20231012114159_update_integrations_tbl.sql
+++ internal/infrastructure/db/migrations/20231012114159_update_integrations_tbl.sql
@@ -1,8 +1,9 @@
 -- +goose Up
 -- +goose StatementBegin
 SET search_path = device_definitions_api, public;
-ALTER TABLE integrations ADD COLUMN points int;
-ALTER TABLE integrations ADD COLUMN manufacturer_token_id numeric(78);
+ALTER TABLE integrations ADD COLUMN points INT NOT NULL DEFAULT 0;
+ALTER TABLE integrations ADD COLUMN manufacturer_token_id INT;
+ALTER TABLE integrations ADD CONSTRAINT manufacturer_token_id_fkey FOREIGN KEY (manufacturer_token_id) REFERENCES device_makes(token_id);
 
 
 -- +goose StatementEnd
@@ -10,6 +11,8 @@ ALTER TABLE integrations ADD COLUMN manufacturer_token_id numeric(78);
 -- +goose Down
 -- +goose StatementBegin
 SET search_path = device_definitions_api, public;
-ALTER TABLE integrations DROP COLUMN points;
+ALTER TABLE integrations DROP CONSTRAINT manufacturer_token_id_fkey;
+ALTER TABLE integrations ALTER COLUMN points DROP NOT NULL;
 ALTER TABLE integrations DROP COLUMN manufacturer_token_id;
+ALTER TABLE integrations DROP COLUMN points;
 -- +goose StatementEnd
