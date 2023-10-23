d3b3b2090d573ea800cea0c5c85333ba70e15bb6
Dylan Moreland <dylan@dimo.zone>
2023-10-23 16:31:13 -0400

    Clean up the migrations a little

--- internal/infrastructure/db/migrations/20231012114159_integrations_points_manufacturer.sql
+++ internal/infrastructure/db/migrations/20231012114159_integrations_points_manufacturer.sql
@@ -3,16 +3,12 @@
 SET search_path = device_definitions_api, public;
 ALTER TABLE integrations ADD COLUMN points INT NOT NULL DEFAULT 0;
 ALTER TABLE integrations ADD COLUMN manufacturer_token_id INT;
-ALTER TABLE integrations ADD CONSTRAINT manufacturer_token_id_fkey FOREIGN KEY (manufacturer_token_id) REFERENCES device_makes(token_id);
-
-
+ALTER TABLE integrations ADD CONSTRAINT integrations_manufacturer_token_id_fkey FOREIGN KEY (manufacturer_token_id) REFERENCES device_makes(token_id);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 SET search_path = device_definitions_api, public;
-ALTER TABLE integrations DROP CONSTRAINT manufacturer_token_id_fkey;
-ALTER TABLE integrations ALTER COLUMN points DROP NOT NULL;
 ALTER TABLE integrations DROP COLUMN manufacturer_token_id;
 ALTER TABLE integrations DROP COLUMN points;
 -- +goose StatementEnd
