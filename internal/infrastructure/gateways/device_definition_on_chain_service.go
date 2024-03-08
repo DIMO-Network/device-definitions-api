4c4b74579c26358113b90a2bef6241a7e0a34a4c
ricardo <chenton147@hotmail.com>
2024-03-07 22:34:06 -0500

    Initial

--- internal/infrastructure/gateways/device_definition_on_chain_service.go
+++ internal/infrastructure/gateways/device_definition_on_chain_service.go
@@ -3,7 +3,6 @@ package gateways
 import (
 	"context"
 	"crypto/ecdsa"
-	"encoding/json"
 	"fmt"
 	"github.com/DIMO-Network/device-definitions-api/internal/config"
 	"github.com/DIMO-Network/device-definitions-api/internal/contracts"
@@ -13,7 +12,6 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/rs/zerolog"
-	"github.com/volatiletech/null/v8"
 	"github.com/volatiletech/sqlboiler/v4/types"
 	"math/big"
 )
@@ -77,52 +75,23 @@ func (e *deviceDefinitionOnChainService) CreateOrUpdate(ctx context.Context, man
 	}
 
 	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, chainID)
-	//auth.Value = big.NewInt(0)
+	auth.Value = big.NewInt(0)
 	auth.Nonce = big.NewInt(int64(nonce))
 	auth.GasLimit = uint64(300000)
 	auth.GasPrice = gasPrice
 	auth.From = fromAddress
-
-	manufacturerId := manufacturerID.Big.Int(new(big.Int))
-
-	queryInstance, err := contracts.NewRegistry(contractAddress, client)
-	if err != nil {
-		return nil, fmt.Errorf("failed create NewRegistry: %w", err)
-	}
-
-	// Validate if manufacturer exists
-	_, err = queryInstance.GetManufacturerNameById(&bind.CallOpts{Context: ctx, Pending: true}, manufacturerId)
-	if err != nil {
-		e.Logger.Info().Msgf("%s", err)
-		return nil, fmt.Errorf("failed get GetManufacturerNameById: %w", err)
-	}
 
 	instance, err := contracts.NewRegistryTransactor(contractAddress, client)
 	if err != nil {
 		return nil, fmt.Errorf("failed create NewRegistryTransactor: %w", err)
 	}
 
-	deviceInputs := contracts.DeviceDefinitionInput{
-		Id:       fmt.Sprintf("%s_%d", dd.ModelSlug, dd.Year),
-		Model:    dd.ModelSlug,
-		Year:     big.NewInt(int64(dd.Year)),
-		Metadata: "",
-		Ksuid:    dd.ID,
+	deviceInputs := []contracts.DeviceDefinitionInput{
+		{Id: fmt.Sprintf("%s_%d", dd.ModelSlug, dd.Year), Model: dd.ModelSlug, Year: big.NewInt(int64(dd.Year)), Metadata: "", Ksuid: dd.ID},
 	}
 
-	if dd.Metadata.Valid {
-		attributes := GetDeviceAttributesTyped(dd.Metadata, "vehicle_info")
-		type deviceAttributes struct {
-			DeviceAttributes []DeviceTypeAttribute `json:"device_attributes"`
-		}
-		deviceAttributesStruct := deviceAttributes{
-			DeviceAttributes: attributes,
-		}
-		jsonData, _ := json.Marshal(deviceAttributesStruct)
-		deviceInputs.Metadata = string(jsonData)
-	}
-
-	tx, err := instance.InsertDeviceDefinition(auth, manufacturerId, deviceInputs)
+	manufacturerId := manufacturerID.Big.Int(new(big.Int))
+	tx, err := instance.InsertDeviceDefinitionBatch(auth, manufacturerId, deviceInputs)
 
 	if err != nil {
 		e.Logger.Info().Msgf("%s", err)
@@ -132,28 +101,4 @@ func (e *deviceDefinitionOnChainService) CreateOrUpdate(ctx context.Context, man
 	trx := tx.Hash().Hex()
 
 	return &trx, nil
-}
-
-func GetDeviceAttributesTyped(metadata null.JSON, key string) []DeviceTypeAttribute {
-	var respAttrs []DeviceTypeAttribute
-	var ai map[string]any
-	if err := metadata.Unmarshal(&ai); err == nil {
-		if ai != nil {
-			if a, ok := ai[key]; ok && a != nil {
-				attributes := ai[key].(map[string]any)
-				for key, value := range attributes {
-					respAttrs = append(respAttrs, DeviceTypeAttribute{
-						Name:  key,
-						Value: fmt.Sprint(value),
-					})
-				}
-			}
-		}
-	}
-	return respAttrs
-}
-
-type DeviceTypeAttribute struct {
-	Name  string `json:"name"`
-	Value string `json:"value"`
 }
