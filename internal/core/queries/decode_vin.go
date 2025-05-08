7b66b6113d2e501c354309ecf48dc39400468ad2
James Reategui <JamesReate@users.noreply.github.com>
2025-05-08 15:16:26 -0400

    japan vin shorter

--- internal/core/queries/decode_vin.go
+++ internal/core/queries/decode_vin.go
@@ -72,7 +72,7 @@ func NewDecodeVINQueryHandler(dbs func() *db.ReaderWriter, vinDecodingService se
 
 func (dc DecodeVINQueryHandler) Handle(ctx context.Context, query mediator.Message) (interface{}, error) {
 	qry := query.(*DecodeVINQuery)
-	if len(qry.VIN) < 13 || len(qry.VIN) > 17 {
+	if len(qry.VIN) < 12 || len(qry.VIN) > 17 {
 		return nil, &exceptions.ValidationError{Err: fmt.Errorf("invalid vin %s", qry.VIN)}
 	}
 	resp := &p_grpc.DecodeVinResponse{}
